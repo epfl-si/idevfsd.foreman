2db0b42d6aae17721ff4569fc065bfdd56e34ccc
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-03-08 10:41:50 +0100

    [fix] `| valuesets`: do nothing on unhashable (e.g. dict) values

--- roles/foreman-idevfsd/filter_plugins/sets.py
+++ roles/foreman-idevfsd/filter_plugins/sets.py
@@ -13,7 +13,13 @@ def valuesets(list_of_dicts):
     valuesets = {}
     for d in list_of_dicts:
         for k in d.keys():
-            valuesets.setdefault(k, set()).add(d[k])
+            valuesets.setdefault(k, set())
+            try:
+                valuesets[k].add(d[k])
+            except TypeError:
+                # No valuesets for dicts etc.
+                pass
+                
     # "Object of type set is not JSON serializable". Sigh
     for k in valuesets.keys():
         valuesets[k] = list(valuesets[k])
