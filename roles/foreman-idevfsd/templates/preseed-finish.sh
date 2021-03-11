af8a6c62437f77275cf5f160d3a317feb7191fd8
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-03-11 13:48:03 +0100

    [feature] One-command reinstall
    ssh as root into a node and type `foreman-reinstall-on-next-reboot` to
    do exactly what it says on the tin.
    
    - Add `Preseed default iPXE` template to Ubuntu 20.04 OS
    - Chain-boot to it from Grub, which as it turns out, is as simple as
    installing the ipxe package and creating a .ipxe script
    - Add a suitable alias into root's `.bash_aliases`

--- roles/foreman-idevfsd/templates/preseed-finish.sh
+++ roles/foreman-idevfsd/templates/preseed-finish.sh
@@ -1,3 +1,5 @@
+set -e -x
+
 <% if !( host_param('epfl_root_authorized_keys').blank? ||
          host_param('epfl_root_authorized_keys').strip.empty? ) %>
 # Add ssh keys from Foreman's 'epfl_root_authorized_keys' host / hostgroup parameter
@@ -17,5 +19,27 @@ command -v restorecon && restorecon -RvF ~root/.ssh || true
 # Install open-vm-tools (https://docs.vmware.com/en/VMware-Tools/11.2.0/com.vmware.vsphere.vmwaretools.doc/GUID-C48E1F14-240D-4DD1-8D4C-25B6EBE4BB0F.html)
 apt-get -qy update
 apt-get -qy install open-vm-tools
+
+# Make it possible to reinstall from Foreman using an ssh command
+apt-get -qy install ipxe
+perl -i -pe 's|GRUB_DEFAULT=.*|GRUB_DEFAULT=saved|' /etc/default/grub
+cat > /boot/ipxe.ipxe <<IPXE_SCRIPT
+#!ipxe
+
+<% iface  = @host.interfaces.first %>
+ifopen net0
+set net0/ip <%= iface.ip %>
+set net0/netmask <%= iface.subnet.mask %>
+set net0/gateway <%= iface.subnet.gateway %>
+set dns <%= iface.subnet.dns_primary %>
+
+chain {{ foreman_frontend_url | replace("https://", "http://") }}unattended/iPXE
+
+IPXE_SCRIPT
+update-grub
+
+cat >> /root/.bash_aliases <<ALIAS
+alias foreman-reinstall-on-next-reboot='grub-reboot "Network boot (iPXE)"'
+ALIAS
 
 <%= snippet_if_exists(@host.hostgroup.to_s + " finish snippet") %>
