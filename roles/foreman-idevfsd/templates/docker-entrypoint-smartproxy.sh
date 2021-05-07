8c1c0c209c9175bb7d8cd4647ea38debb9b19804
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-05-07 16:59:44 +0200

    [rewrite] smartproxy container entry point for idempotence
    I saw that there were three [callback_foreman] in
    /etc/ansible/ansible.cfg today; not sure in which circumstances this
    happens (perhaps a reboot or stop/start of the smartproxy-prod
    container?)

--- roles/foreman-idevfsd/templates/docker-entrypoint-smartproxy.sh
+++ roles/foreman-idevfsd/templates/docker-entrypoint-smartproxy.sh
@@ -5,6 +5,7 @@ set -e -x
 cd /usr/src/app
 if grep :foreman_url: config/settings.yml; then
     # https://theforeman.org/plugins/foreman_ansible/3.x/index.html#2.1Ansiblecallback
+    sed -i -n -n '/callback_foreman/q;p' /etc/ansible/ansible.cfg
     cat >>/etc/ansible/ansible.cfg <<CALLBACK_FOREMAN
 [callback_foreman]
 url =$( grep :foreman_url: config/settings.yml | cut -d: -f3- | sed 's|"||g' )
